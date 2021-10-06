10dfbfa918453c1a148df3563f16dda6fc5054b0
eulalie <eulaliegee@gmail.com>
2021-10-05 23:08:13 -0400

    updated folders with comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GT-Book-Search-Engine
+
+
+## User Story
+
+```md
+AS AN avid reader
+I WANT to search for new books to read
+SO THAT I can keep a list of books to purchase
+```
---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+
+   
+// BRING IN REACT MODULES
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// BRING IN BOOTSTRAP AND STYLESHEET
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// BRING IN APP COMPONENT
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

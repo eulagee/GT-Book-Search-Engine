e5d1d1949b401ad0994573fa22ce62abda9e27d8
eulalie <eulaliegee@gmail.com>
2021-10-05 23:46:15 -0400

    updated schemas folder

---
+++ schemas/resolvers.js
@@ -0,0 +1,79 @@
+// BRING IN AUTHENTICATION ERROR MODULE, USER MODEL AND SIGNTOKEN MIDDLEWARE
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+
+                return userData
+            }
+            // THROW ERROR FOR NEED TO LOG IN
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+
+    Mutation: {
+        // ADD A USER TO DATABASE WITH USERNAME, EMAIL AND PASSWORD, AND CREATE A TOKEN
+        addUser: async(parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // LOGIN A USER WITH THEIR EMAIL AND PASSWORD
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            // IF NO USER FOUND IN DATABASE, THROW ERROR
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!');
+            }
+
+            // USE CUSTOM METHOD TO CHECK ENTERED PASSWORD AGAINST HASHED DB PASSWORD
+            const correctPw = await user.isCorrectPassword(password);
+
+            // THROW ERROR FOR INCORRECT PASSWORD
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            // CREATE TOKEN FOR USER
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        // Add a third argument to the resolver to access data in our `context`
+        saveBook: async(parent, { bookId, authors, description, image, link, title }, context) => {
+            // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+            if (context.user) {
+                // FIND LOGGED IN USER IN DATABASE, AND ADD THE BOOK TO THEIR SAVEDBOOKS
+                return User.findOneAndUpdate({ _id: context.user._id }, {
+                    $addToSet: { savedBooks: { bookId, authors, description, image, link, title } },
+                }, {
+                    new: true,
+                    runValidators: true,
+                });
+            }
+            // If user attempts to execute this mutation and isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        // Make it so a logged in user can only remove a book from their own profile
+        removeBook: async(parent, { bookId }, context) => {
+            if (context.user) {
+                // FIND LOGGED IN USER IN DATABASE AND REMOVE THE SELECTED BOOK FROM THEIR SAVEDBOOKS
+                const updatedUser = await User.findOneAndUpdate({ _id: context.user._id }, { $pull: { savedBooks: { bookId } } }, { new: true });
+
+                return updatedUser;
+            }
+            // THROW ERROR IF USER NOT LOGGED IN
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
---
+++ schemas/typeDefs.js
@@ -0,0 +1,38 @@
+// BRING IN GRAPHQL FOR APOLLO-SERVER-EXPRESS
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookId: String!, authors: [String], description: String, image: String, link: String, title: String!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+// EXPORT TYPEDEFS FOR USE IN APP
+module.exports = typeDefs;
